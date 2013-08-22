b4cd49b50e7794e295a2c117b0a76480c7378f78
L Lumbreras <llumbreras@gmail.com>
2013-08-22 13:42:25 -0700

    Add script for find/replace

---
+++ find2replace.rb
@@ -0,0 +1,25 @@
+# Dir is the working directory
+# Use glob() method to return an array of filename with txt suffix
+# each filename is verified as file
+# File is assigned to old_filename
+# If old_filename includes the old_word use gsub to substitute
+# Use File rename() method for renaming files
+
+
+
+
+def find2replace (old_word,new_word)
+  Dir.glob("*.txt").each do |file|
+    next unless File.file?(file)
+      old_filename = File.basename(file)
+    if old_filename.include?(old_word)
+      new_filename = File.basename(file).gsub(old_word,new_word)
+      File.rename(file, new_filename)
+      puts "Renamed #{file} to #{new_filename}"
+    end
+  end
+end
+
+find2replace("old word","new word")
+
+
