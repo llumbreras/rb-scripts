0fc566b00fb15adc941a862198ab05d9774146a5
L Lumbreras <llumbreras@gmail.com>
2013-08-22 21:16:34 -0700

    Modify to include number of entries

--- comparedifference.rb
+++ comparedifference.rb
@@ -4,9 +4,10 @@ def compare_difference (file01, file02)
   listing01 = File.open(file01).readlines
   listing02 = File.open(file02).readlines
 
+  puts "#{file02} has #{listing02.length} lines."
   puts "The following files have been included:"
   puts listing02 - listing01
-
+  puts "#{file01} has #{listing01.length} lines."
   puts "The following files have been removed:"
   puts listing01 - listing02
 end
